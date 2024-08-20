769fe1526d267f6047c3037ffbf0d33c3877cf8d
Estikno <95084542+Estikno@users.noreply.github.com>
2024-08-20 12:50:06 +0200

    Able to get resources and mutate them

--- crates/axle_ecs/src/lib.rs
+++ crates/axle_ecs/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::any::Any;
+
+use resource::Resource;
+
+mod resource;
+
+#[derive(Default)]
+pub struct World {
+    resources: Resource
+}
+
+impl World {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn add_resource(&mut self, resource_data: impl Any) {
+        self.resources.add(resource_data);
+    }
+
+    pub fn get_resource<T: Any>(&self) -> Option<&T> {
+        self.resources.get_ref::<T>()
+    }
+
+    /// Query for a resource and get a mutable reference to it.
+    /// 
+    /// The type of the resource must be added in so that it can be found.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// use axle_ecs::World;
+    /// 
+    /// let mut world = World::new();
+    /// world.add_resource(10_i32);
+    /// 
+    /// {
+    ///     let resource = world.get_resource_mut::<i32>().unwrap();
+    ///     *resource += 1;
+    /// }
+    /// 
+    /// let resource = world.get_resource::<i32>().unwrap();
+    /// 
+    /// assert_eq!(*resource, 11);
+    /// ```
+    pub fn get_resource_mut<T: Any>(&mut self) -> Option<&mut T> {
+        self.resources.get_mut::<T>()
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
