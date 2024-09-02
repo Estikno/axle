4304d290a08e65e7709e5c82e55a951b4ddaa3d7
Estikno <95084542+Estikno@users.noreply.github.com>
2024-09-02 09:20:43 +0200

    Finish adding integration tests for systems ECS

--- tests/ecs/systems.rs
+++ tests/ecs/systems.rs
@@ -175,6 +175,84 @@ fn delete_a_system() -> Result<()> {
     Ok(())
 }
 
+#[test]
+fn more_systems_at_the_same_time() -> Result<()> {
+    let mut world = World::new();
+
+    world.register_component::<Location>();
+    world.register_component::<Speed>();
+
+    world
+        .create_entity()
+        .with_component(Location(0.0, 0.0))?
+        .with_component(Speed(10.0))?;
+
+    world
+        .create_entity()
+        .with_component(Speed(20.0))?;
+
+    world
+        .create_entity()
+        .with_component(Location(20.0, 20.0))?;
+
+    world
+        .create_entity()
+        .with_component(Location(10.0, 10.0))?
+        .with_component(Speed(5.0))?;
+
+    world
+        .create_system(&update_speed)
+        .with_component::<Speed>()?;
+
+    world
+        .create_system(&update_location)
+        .with_component::<Location>()?
+        .with_component::<Speed>()?;
+
+    world.run_all_systems()?;
+
+    let query = world.query()
+        .with_component::<Speed>()?
+        .run();
+
+    let speeds = &query.1[0];
+
+    let wrapped_first_speed = speeds[0].borrow();
+    let first_speed = wrapped_first_speed.downcast_ref::<Speed>().unwrap();
+    assert_eq!(first_speed.0, 20.0);
+
+    let wrapped_second_speed = speeds[1].borrow();
+    let second_speed = wrapped_second_speed.downcast_ref::<Speed>().unwrap();
+    assert_eq!(second_speed.0, 30.0);
+
+    let wrapped_third_speed = speeds[2].borrow();
+    let third_speed = wrapped_third_speed.downcast_ref::<Speed>().unwrap();
+    assert_eq!(third_speed.0, 15.0);
+
+    let query = world.query()
+        .with_component::<Location>()?
+        .run();
+
+    let locations = &query.1[0];
+
+    let wrapped_first_location = locations[0].borrow();
+    let first_location = wrapped_first_location.downcast_ref::<Location>().unwrap();
+    assert_eq!(first_location.0, 20.0);
+    assert_eq!(first_location.1, 20.0);
+
+    let wrapped_second_location = locations[1].borrow();
+    let second_location = wrapped_second_location.downcast_ref::<Location>().unwrap();
+    assert_eq!(second_location.0, 20.0);
+    assert_eq!(second_location.1, 20.0);
+
+    let wrapped_third_location = locations[2].borrow();
+    let third_location = wrapped_third_location.downcast_ref::<Location>().unwrap();
+    assert_eq!(third_location.0, 25.0);
+    assert_eq!(third_location.1, 25.0);
+
+    Ok(())
+}
+
 fn update_location(entities: &Vec<QueryEntity>) -> Result<()> {
     for entity in entities {
         let mut location = entity.get_component_mut::<Location>()?;
