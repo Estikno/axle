9bde7a9895a8da93c981e24fb2a2c54f01d23597
Estikno <95084542+Estikno@users.noreply.github.com>
2024-09-22 18:03:34 +0200

    Modifgy application script to make it better

--- crates/axle_render/src/renderer.rs
+++ crates/axle_render/src/renderer.rs
@@ -8,55 +8,42 @@ use crate::config::RenderConfig;
 use crate::custom_errors::CustomErrors;
 use crate::primitives::renderable::Renderable;
 
-//sdl2
-use sdl2::render::Canvas;
-use sdl2::video::Window;
-use sdl2::Sdl;
-use sdl2::VideoSubsystem;
+pub fn new(world: &mut World) -> Result<()> {
+    // Get config form the ecs's resources. If there is nothing panic as the config must be
+    // there.
+    let config = world
+        .get_resource::<RenderConfig>()
+        .ok_or(CustomErrors::RenderConfigNotRegistered)
+        .unwrap();
 
-pub struct Renderer {
-    sdl_context: Sdl,
-    canvas: Canvas<Window>,
-    video_subsystem: VideoSubsystem,
+    // SDL initialization
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window(&config.title, config.resolution.0, config.resolution.1)
+        .position_centered()
+        .build()
+        .unwrap();
+    let canvas = window.into_canvas().build().unwrap();
+
+    // Resgiter resources
+    world.add_resource(sdl_context);
+    world.add_resource(video_subsystem);
+    world.add_resource(canvas);
+
+    // Register render function in the ECS
+    world
+        .create_system(&render)
+        .with_component::<Transform>()?
+        .with_component::<Box<dyn Renderable>>()?;
+
+    Ok(())
 }
 
-impl Renderer {
-    pub fn new(world: &mut World) -> Result<Self> {
-        // Get config form the ecs's resources. If there is nothing panic as the config must be
-        // there.
-        let config = world
-            .get_resource::<RenderConfig>()
-            .ok_or(CustomErrors::RenderConfigNotRegistered)
-            .unwrap();
-
-        // SDL initialization
-        let sdl_context = sdl2::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-        let window = video_subsystem
-            .window(&config.title, config.resolution.0, config.resolution.1)
-            .position_centered()
-            .build()
-            .unwrap();
-        let canvas = window.into_canvas().build().unwrap();
-
-        // Register render function in the ECS
-        world
-            .create_system(&Renderer::render)
-            .with_component::<Transform>()?
-            .with_component::<Box<dyn Renderable>>()?;
-
-        Ok(Self {
-            sdl_context,
-            video_subsystem,
-            canvas,
-        })
+pub fn render(entities: &Vec<QueryEntity>, resource: &mut Resources) -> Result<()> {
+    for entities in entities {
+        let transform = entities.get_component::<Transform>()?;
     }
 
-    pub fn render(entities: &Vec<QueryEntity>, resource: &mut Resources) -> Result<()> {
-        for entities in entities {
-            let transform = entities.get_component::<Transform>()?;
-        }
-
-        Ok(())
-    }
+    Ok(())
 }
