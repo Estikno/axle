df64b1995acd92c9699a5fd8ec67328602c41a79
Estikno <95084542+Estikno@users.noreply.github.com>
2024-10-09 19:21:21 +0200

    Update little details

--- crates/axle_render/src/renderer.rs
+++ crates/axle_render/src/renderer.rs
@@ -9,12 +9,11 @@ use crate::custom_errors::CustomErrors;
 use crate::primitives::renderable::Renderable;
 
 pub fn new(world: &mut World) -> Result<()> {
-    // Get config form the ecs's resources. If there is nothing panic as the config must be
+    // Get config form the ecs's resources. If there is nothing return an error as the config must be
     // there.
     let config = world
         .get_resource::<RenderConfig>()
-        .ok_or(CustomErrors::RenderConfigNotRegistered)
-        .unwrap();
+        .ok_or(CustomErrors::RenderConfigNotRegistered)?;
 
     // SDL initialization
     let sdl_context = sdl2::init().unwrap();
@@ -40,7 +39,8 @@ pub fn new(world: &mut World) -> Result<()> {
     Ok(())
 }
 
-pub fn render(entities: &Vec<QueryEntity>, resource: &mut Resources) -> Result<()> {
+// TODO:: Finish the render function
+fn render(entities: &Vec<QueryEntity>, resources: &mut Resources) -> Result<()> {
     for entities in entities {
         let transform = entities.get_component::<Transform>()?;
     }
